2c13946b545abd47519904b8a09a4c8157923c84
krista-chan <qbotdev84@gmail.com>
2021-12-20 13:33:44 +0000

    Huge refactor and start adding auth

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
 #![allow(dead_code)]
 
-pub mod packet;
-pub mod snowflake;
+//! # spwnmsg-core
+//! 
+//! ## Typedefs and basic functions for interacting with spwnmsg
+
+pub use tokio;
+
+pub mod base_types;
+
+// #[cfg(feature = "server")]
+pub mod server;
+
+// #[cfg(feature = "client")]
+pub mod client;
